6c5913ce437b0af2b0ca1201ba6cf676c8e31e9b
ThainhatTien <thainhattien3011@gmail.com>
2024-06-16 00:11:25 +0700

    NTF-Design-Connect

---
+++ src/main/java/com/web/app/NftDesignConnectApplication.java
@@ -0,0 +1,13 @@
+package com.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftDesignConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftDesignConnectApplication.class, args);
+	}
+
+}
